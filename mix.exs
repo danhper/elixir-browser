1c8f9e63d150c7a55665c1da5c6b60ea0de77cf3
Daniel Perez <daniel@perez.sh>
2024-06-26 23:32:45 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Browser.Mixfile do
 
   def project do
     [app: :browser,
-     version: "0.5.1",
+     version: "0.5.2",
      name: "browser",
      source_url: "https://github.com/danhper/elixir-browser",
      homepage_url: "https://github.com/danhper/elixir-browser",
